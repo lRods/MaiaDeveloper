0abac55b8605e921ec590464b5c68f433dbbdd12
Rodrigo Maia <llrodgs@gmail.com>
2024-07-21 11:33:10 -0300

    Escurecendo fundo do titulo

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
     console.log( "ready!" );
 
     ajustarQuadrados();
+
+    console.log(getBelowElements($("#title-nome")[0]))
 });
 
 $(window).resize(function(){
@@ -14,11 +16,28 @@ function ajustarQuadrados() {
     console.log(h)
     console.log(w)
 
+    let extra = 5;
     let quantQuadradosColuna = 10;
-    let alturaQuadrado = (h / quantQuadradosColuna) - 4;
-    let quantMaxQuadradosLinha = w / alturaQuadrado;
-    
-    console.log(alturaQuadrado)
+    let alturaQuadrado = h / quantQuadradosColuna;
+    let quantMaxQuadradosLinha = w / alturaQuadrado + extra;
+
+    let linhaHtml = "<div class='linha'></div>";    
+    let quadradoHtml = "<span class='quadrado'></span>";
+
+    for (let i = 0; i < quantQuadradosColuna + extra; i++) {
+        if (!$("#quadro .linha")[i])
+             $("#quadro").append(linhaHtml)
+
+        let linha = $("#quadro .linha")[i];
+        let quadrados = $(linha).find(".quadrado");
+        if (quadrados.length < quantMaxQuadradosLinha) {
+            for (let j = quadrados.length; j < quantMaxQuadradosLinha; j++) 
+                $(linha).append(quadradoHtml)
+        } else if (quadrados.length > quantMaxQuadradosLinha) {
+            for (let j = quadrados.length; j > quantMaxQuadradosLinha; j--) 
+                $(linha).find(".quadrado").last().remove()
+        }
+    }
     
     $(".quadrado").each(function() {
         $(this).height(alturaQuadrado)
@@ -33,10 +52,13 @@ function ajustarQuadrados() {
 
         loop(this);
     });
+
 }
 
 function loop(elm) {
-    let opacityVal = getRandomInt(2, 30);
+    let maxOpacity = verificarQuadradoAbaixo(elm) ? 0 : 15;
+
+    let opacityVal = getRandomInt(2, maxOpacity);
     let durationVal = getRandomInt(2, 5) * 1000;
     
     $(elm).animate({
@@ -50,4 +72,33 @@ function getRandomInt(min, max) {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
     return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+}
+
+function getBelowElements(el) {
+    const sourceBounding = el.getBoundingClientRect();
+    let belowElements = [];
+
+    for (const currentElement of $(".quadrado")) {
+        const targetBounding = currentElement.getBoundingClientRect();
+
+        // Se todas as condições forem falsas, é porque está tendo uma sobreposição
+        if (!(sourceBounding.right < targetBounding.left ||
+            // sourceBounding.left > targetBounding.right ||
+            // sourceBounding.bottom < targetBounding.top ||
+            sourceBounding.top > targetBounding.bottom)) {
+            belowElements = [...belowElements, currentElement]; // Adiciona o elemento atual ao array de elementos sobrepostos
+        }
+    }
+
+    return belowElements; // Retorna o array de elementos sobrepostos
+}
+
+function verificarQuadradoAbaixo(quadrado) {
+    const quadradoBounding = quadrado.getBoundingClientRect(); 
+    const tituloBounding = $("#title-nome")[0].getBoundingClientRect();
+
+    return !(quadradoBounding.right < tituloBounding.left ||
+            //  quadradoBounding.left > tituloBounding.right ||
+            //  quadradoBounding.bottom < tituloBounding.top ||
+             quadradoBounding.top > tituloBounding.bottom)
 }
